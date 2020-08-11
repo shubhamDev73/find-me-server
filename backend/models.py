b1b42b596e3d0098cf15301b84b687f0f847631b
Shubham Gupta <shubham0209@gmail.com>
2020-08-11 21:06:42 +0530

    Added login, register, logout

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        profile = Profile.objects.create(user=instance)
+        profile.token = create_token()
+        profile.save()
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.token = create_token()
+    instance.profile.save()
+
+def create_token(length=100):
+    import hashlib
+    import random
+    import string
+
+    random_string = ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
+    present = True
+    while present:
+        try:
+            token = hashlib.sha256(random_string.encode('utf-8')).hexdigest()
+            Profile.objects.get(token=token)
+        except Profile.DoesNotExist:
+            present = False
+    return token
--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+@csrf_exempt
+def register(request):
+    response = {'error': ''}
+    if request.method == "POST":
+        try:
+            User.objects.get(username=request.POST['username'])
+            response['error'] = 'username already exists'
+        except User.DoesNotExist:
+            user = User.objects.create(username=request.POST['username'], password=request.POST['password'])
+            user.save()
+            response['token'] = user.profile.token
+    else:
+        response['error'] = 'invalid request'
+    return JsonResponse(response)
+
+@csrf_exempt
+def login(request):
+    response = {'error': ''}
+    if request.method == "POST":
+        try:
+            user = User.objects.get(username=request.POST['username'], password=request.POST['password'])
+            response['token'] = user.profile.token
+        except User.DoesNotExist:
+            response['error'] = 'invalid login'
+    else:
+        response['error'] = 'invalid request'
+    return JsonResponse(response)
+
+@csrf_exempt
+def logout(request):
+    response = {'error': ''}
+    if request.method == "POST":
+        pass
+    else:
+        response['error'] = 'invalid request'
+    return JsonResponse(response)
