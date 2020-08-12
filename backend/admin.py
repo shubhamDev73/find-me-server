a4044ed12acc3b424a5814fd52c29af61f6ab138
Shubham Gupta <shubham0209@gmail.com>
2020-08-12 17:14:37 +0530

    Registered profile in admin. Can expire tokens from there

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    fields = ['user']
+    list_display = ['user', 'expired']
+
+    def expire_tokens(self, request, queryset):
+        queryset.update(expired=True)
+    expire_tokens.short_description = 'Expire auth tokens'
+
+    actions = [expire_tokens]
